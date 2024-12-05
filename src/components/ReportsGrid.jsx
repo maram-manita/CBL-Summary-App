adf0f57b0b4b5537a177f7974c2b04a2338e11d8
maram-manita <itstd.4190@uob.edu.ly>
2024-12-05 21:25:00 +0200

    Finished styling elements for inital page

--- src/components/ReportsGrid.jsx
+++ src/components/ReportsGrid.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { Button, Checkbox } from "@mui/material";
+import { Button, Checkbox, Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import "../App.css";
 
 const paginationModel = { page: 0, pageSize: 8 };
 const ReportsGrid = ({
@@ -10,7 +11,8 @@ const ReportsGrid = ({
   handleFileSelection,
   handleViewPdf,
 }) => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const isRTL = i18n.language === "ar";
 
   return (
     <DataGrid
@@ -29,30 +31,62 @@ const ReportsGrid = ({
             <Checkbox
               checked={selectedFiles.includes(params.row.id)}
               onChange={() => handleFileSelection(params.row.id)}
+              sx={{
+                color: selectedFiles.includes(params.row.id)
+                  ? "#2683E8"
+                  : "default",
+                "&.Mui-checked": {
+                  color: "#2683E8", // Customize the checkbox color when checked
+                },
+              }}
             />
           ),
         },
-        { field: "name", headerName: "File Name", flex: 1 },
+        {
+          field: "name",
+          headerName: "File Name",
+          flex: 1,
+          renderCell: (params) => (
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              {params.value}
+            </Box>
+          ),
+        },
         {
           field: "actions",
           headerName: "Actions",
           width: 150,
           renderCell: (params) => (
-            <Button
-              onClick={() => handleViewPdf(params.row.pdfPath)}
+            <Box
               sx={{
-                color: "white",
-                border: "1px solid #262626",
-                padding: "4px 12px",
-                borderRadius: 8,
-                color: "#262626",
-                fontWeight: "bold",
-                background: "none",
+                width: "100%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
               }}
-              elevation={0}
             >
-              {t("view_button_text")}
-            </Button>
+              <Button
+                onClick={() => handleViewPdf(params.row.pdfPath)}
+                sx={{
+                  border: "1px solid #262626",
+                  padding: "4px 12px",
+                  borderRadius: 4,
+                  color: "#262626",
+                  fontWeight: "bold",
+                  background: "none",
+                }}
+                elevation={0}
+              >
+                {t("view_button_text")}
+              </Button>
+            </Box>
           ),
         },
       ]}
@@ -61,9 +95,14 @@ const ReportsGrid = ({
         "& .MuiDataGrid-columnHeaders": {
           display: "none", // Hide header
         },
-        overflow: "hidden",
+        "& .MuiDataGrid-row:hover": {
+          backgroundColor: "#F9FAFB", // Change the hover background color of the rows
+        },
+        overflowY: "hidden",
+        overflowX: "hidden",
       }}
       disableVirtualization
+      selectionModel={[]}
     />
   );
 };
