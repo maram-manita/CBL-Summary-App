c2872bbc0a116ca0375d8f77bb74d5b00401b1e2
maram-manita <itstd.4190@uob.edu.ly>
2024-12-07 20:34:48 +0200

    Implemented summarization funcationality

--- src/components/ReportsGrid.jsx
+++ src/components/ReportsGrid.jsx
@@ -11,99 +11,100 @@ const ReportsGrid = ({
   handleFileSelection,
   handleViewPdf,
 }) => {
-  const { t, i18n } = useTranslation();
-  const isRTL = i18n.language === "ar";
+  const { t } = useTranslation();
 
   return (
-    <DataGrid
-      rows={filteredData.map((row, index) => ({
-        id: index + 1,
-        name: row.name,
-        pdfPath: row.pdf_path,
-        mdPath: row.md_path,
-      }))}
-      columns={[
-        {
-          field: "select",
-          headerName: "",
-          width: 50,
-          renderCell: (params) => (
-            <Checkbox
-              checked={selectedFiles.includes(params.row.id)}
-              onChange={() => handleFileSelection(params.row.id)}
-              sx={{
-                color: selectedFiles.includes(params.row.id)
-                  ? "#2683E8"
-                  : "default",
-                "&.Mui-checked": {
-                  color: "#2683E8", // Customize the checkbox color when checked
-                },
-              }}
-            />
-          ),
-        },
-        {
-          field: "name",
-          headerName: "File Name",
-          flex: 1,
-          renderCell: (params) => (
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                height: "100%",
-              }}
-            >
-              {params.value}
-            </Box>
-          ),
-        },
-        {
-          field: "actions",
-          headerName: "Actions",
-          width: 150,
-          renderCell: (params) => (
-            <Box
-              sx={{
-                width: "100%",
-                height: "100%",
-                display: "flex",
-                justifyContent: "flex-end",
-                alignItems: "center",
-              }}
-            >
-              <Button
-                onClick={() => handleViewPdf(params.row.pdfPath)}
+    <Box p={2}>
+      <DataGrid
+        rows={filteredData.map((row, index) => ({
+          id: index + 1,
+          name: row.name,
+          pdfPath: row.pdf_path,
+          mdPath: row.md_path,
+        }))}
+        columns={[
+          {
+            field: "select",
+            headerName: "",
+            width: 50,
+            renderCell: (params) => (
+              <Checkbox
+                checked={selectedFiles.includes(params.row.id)}
+                onChange={() => handleFileSelection(params.row.id)}
                 sx={{
-                  border: "1px solid #262626",
-                  padding: "4px 12px",
-                  borderRadius: 4,
-                  color: "#262626",
-                  fontWeight: "bold",
-                  background: "none",
+                  color: selectedFiles.includes(params.row.id)
+                    ? "#2683E8"
+                    : "default",
+                  "&.Mui-checked": {
+                    color: "#2683E8", // Customize the checkbox color when checked
+                  },
                 }}
-                elevation={0}
+              />
+            ),
+          },
+          {
+            field: "name",
+            headerName: "File Name",
+            flex: 1,
+            renderCell: (params) => (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  height: "100%",
+                }}
               >
-                {t("view_button_text")}
-              </Button>
-            </Box>
-          ),
-        },
-      ]}
-      initialState={{ pagination: { paginationModel } }}
-      sx={{
-        "& .MuiDataGrid-columnHeaders": {
-          display: "none", // Hide header
-        },
-        "& .MuiDataGrid-row:hover": {
-          backgroundColor: "#F9FAFB", // Change the hover background color of the rows
-        },
-        overflowY: "hidden",
-        overflowX: "hidden",
-      }}
-      disableVirtualization
-      selectionModel={[]}
-    />
+                {params.value}
+              </Box>
+            ),
+          },
+          {
+            field: "actions",
+            headerName: "Actions",
+            width: 150,
+            renderCell: (params) => (
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  alignItems: "center",
+                }}
+              >
+                <Button
+                  onClick={() => handleViewPdf(params.row.pdfPath)}
+                  sx={{
+                    border: "1px solid #262626",
+                    padding: "4px 12px",
+                    borderRadius: 4,
+                    color: "#262626",
+                    fontWeight: "bold",
+                    background: "none",
+                  }}
+                  elevation={0}
+                >
+                  {t("view_button_text")}
+                </Button>
+              </Box>
+            ),
+          },
+        ]}
+        initialState={{ pagination: { paginationModel } }}
+        sx={{
+          "& .MuiDataGrid-columnHeaders": {
+            display: "none", // Hide header
+          },
+          "& .MuiDataGrid-row:hover": {
+            backgroundColor: "#F9FAFB", // Change the hover background color of the rows
+          },
+          overflowY: "hidden",
+          overflowX: "hidden",
+        }}
+        disableVirtualization
+        selectionModel={[]}
+      />
+    </Box>
   );
 };
 
---
+++ src/components/Summary.jsx
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  Grid2 as Grid,
+  CircularProgress,
+} from "@mui/material";
+import { useTranslation } from "react-i18next";
+import { MuiMarkdown, getOverrides } from "mui-markdown";
+import axios from "axios";
+
+const Summary = ({ selectedFiles, language, handleFeedback, data }) => {
+  const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
+  const [summary, setSummary] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchMarkdownFiles = async () => {
+    try {
+      const selectedFileData = data.filter((file) =>
+        selectedFiles.includes(file.id)
+      );
+
+      const markdownTexts = selectedFileData.map((file) => file.md_content);
+      const combinedText = markdownTexts.join("\n\n");
+
+      console.log(language);
+      let prompt;
+      if (language == "en") {
+        prompt = `
+          Summarize the following text in English using organized headings and bullet points.
+          Add Markdown tables for key data if applicable.
+          ${combinedText}
+        `;
+      } else {
+        prompt = ` قم بتلخيص النص التالي باللغة العربية مع استخدام نقاط وعناوين منظمة وشاملة.
+            إذا كان ذلك مناسبًا، أضف جداول بتنسيق Markdown لعرض البيانات الهامة.
+            ${combinedText}
+            `;
+      }
+
+      console.log("Prompt for summarization:", prompt);
+
+      return prompt; // Return the prompt for further use
+    } catch (error) {
+      console.error("Error processing Markdown content:", error);
+      throw new Error("Error processing Markdown content.");
+    }
+  };
+
+  // Function to fetch the summary using the Gemini API
+  const fetchSummary = async () => {
+    setLoading(true);
+    try {
+      // Fetch the combined Markdown content as a prompt
+      const prompt = await fetchMarkdownFiles();
+
+      // Make the API call to summarize the content
+      const response = await axios({
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
+        method: "post",
+        data: {
+          contents: [
+            {
+              parts: [
+                {
+                  text: prompt,
+                },
+              ],
+            },
+          ],
+        },
+      });
+
+      // Display feedback and update the summary state
+      handleFeedback("success", t("summary_success_message"));
+      setSummary(response.data.candidates[0].content.parts[0].text);
+
+      console.log("Summary Response:", response);
+    } catch (error) {
+      console.error("Error fetching summary:", error);
+      handleFeedback("error", t("summary_error_message"));
+    } finally {
+      setLoading(false); // Always execute after try or catch
+    }
+  };
+
+  const { t } = useTranslation();
+  return (
+    <Grid item size={7}>
+      <Card variant="outlined" style={{ padding: 16, height: "100%" }}>
+        <Typography sx={{ fontSize: "28px", fontWeight: "bold" }}>
+          {t("summary_label")}
+        </Typography>
+        {!loading ? (
+          <Button
+            disabled={selectedFiles.length === 0}
+            onClick={fetchSummary}
+            style={{ marginTop: 16 }}
+            sx={{
+              backgroundColor: "#2683e8",
+              padding: "8px 12px",
+              borderRadius: 2,
+              color: "white",
+              fontWeight: "bold",
+              "&.Mui-disabled": {
+                backgroundColor: "#E0E0E0",
+                color: "#B1ACA6",
+              },
+            }}
+          >
+            {t("summarize_button_text")}
+          </Button>
+        ) : (
+          <CircularProgress size="30px" sx={{ marginTop: 2 }} />
+        )}
+
+        <Typography sx={{ padding: "12px", marginTop: "12px" }}>
+          <MuiMarkdown
+            overrides={{
+              ...getOverrides({}), // This will keep the other default overrides.
+              h1: {
+                component: "p",
+                props: {
+                  style: {
+                    fontSize: "32px",
+                    fontWeight: "bold",
+                    wordSpacing: "-0.1em",
+                    margin: "8px 0",
+                  },
+                },
+              },
+              h2: {
+                component: "p",
+                props: {
+                  style: {
+                    fontSize: "24px",
+                    fontWeight: "bold",
+                    wordSpacing: "-0.1em",
+                    margin: "4px 0",
+                  },
+                },
+              },
+              h3: {
+                component: "p",
+                props: {
+                  style: {
+                    fontSize: "20px",
+                    fontWeight: "bold",
+                    wordSpacing: "-0.1em",
+                    margin: "4px 0",
+                  },
+                },
+              },
+              p: {
+                component: "p",
+                props: {
+                  style: {
+                    // marginTop: "4px",
+                  },
+                },
+              },
+              ul: {
+                component: "p",
+                props: {
+                  style: {
+                    marginRight: "12px",
+                  },
+                },
+              },
+            }}
+          >
+            {summary}
+          </MuiMarkdown>
+        </Typography>
+      </Card>
+    </Grid>
+  );
+};
+
+export default Summary;
